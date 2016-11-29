cc16c02a63b75bd9de3d9474809026c3486197ce
oscar <yuszy@outlook.com>
2016-11-29 21:28:04 +1100

    Test

--- RandomForestsSenAnalysis/__init__.py
+++ RandomForestsSenAnalysis/__init__.py
@@ -6,16 +6,16 @@ import ctypes
 # the path of current working directory
 # the interface 'invoke_rf' is called in
 # controller file which is under its parent dir.
-PATH = os.path.abspath("RandomForestsSenAnalysis/libRF.so")
-RANDOM_FORESTS = ctypes.CDLL(PATH)
-RANDOM_FORESTS.Train.argtypes = [ctypes.c_int]
-RANDOM_FORESTS.Test.argtypes = []
+#PATH = os.path.abspath("RandomForestsSenAnalysis/libRF.so")
+#RANDOM_FORESTS = ctypes.CDLL(PATH)
+#RANDOM_FORESTS.Train.argtypes = [ctypes.c_int]
+#RANDOM_FORESTS.Test.argtypes = []
 
-def invoke_rf_train(num_trees):
-    """Call random forest training method."""
-    RANDOM_FORESTS.Train(ctypes.c_int(num_trees))
+#def invoke_rf_train(num_trees):
+#    """Call random forest training method."""
+#    RANDOM_FORESTS.Train(ctypes.c_int(num_trees))
 
-def invoke_rf_test():
-    """Call random forest test methon."""
-    RANDOM_FORESTS.Test()
+#def invoke_rf_test():
+#    """Call random forest test methon."""
+#    RANDOM_FORESTS.Test()
 
--- application.py
+++ application.py
@@ -2,7 +2,7 @@
 """Wrap and expose API interfaces for sentiment analysis."""
 
 from flask import Flask
-from RandomForestsSenAnalysis import invoke_rf_test, invoke_rf_train
+#from RandomForestsSenAnalysis import invoke_rf_test, invoke_rf_train
 
 APP = Flask(__name__)
 
@@ -10,14 +10,14 @@ APP = Flask(__name__)
 
 def rf_train():
     """Random forest training api."""
-    invoke_rf_train(5)
+    #invoke_rf_train(5)
     return 'rf training finished'
 
 @APP.route('/rf_test')
 
 def rf_test():
     """Random forest test api."""
-    invoke_rf_test()
+    #invoke_rf_test()
     return 'rf test finished'
 
 if __name__ == '__main__':
